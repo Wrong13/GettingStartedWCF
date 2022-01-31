cb70f34ceaa8e680acd01ddf3981b578266fa64d
Wrong <ilak71363@gmail.com>
2022-01-31 20:25:53 +0300

    Добавьте файлы проекта.

---
+++ GettingStarted/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GettingStarted
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class Service1 : ICalculator
+    {
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            Console.WriteLine("Add({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+        public double Substract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
+    }
+}
---
+++ GettingStartedHost/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using GettingStarted;
+namespace GettingStartedHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/GettingStarted/");
+
+            ServiceHost selfHost = new ServiceHost(typeof(Service1), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "Калькулятор");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("Служба запущена?");
+
+                Console.ReadKey();
+                selfHost.Close();
+            }
+            catch (CommunicationException e)
+            {
+                Console.WriteLine("Ошибка {0}", e.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
